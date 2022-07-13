f8bc37c52465c632506c4e7eac8ce71b81661811
mjhines2 <mjhines2@gmail.com>
2022-07-13 17:15:24 +1000

    Initial commit

---
+++ src/main/java/net/mjhines2/blowdartmod/BlowDartMod.java
@@ -0,0 +1,15 @@
+package net.mjhines2.blowdartmod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BlowDartMod implements ModInitializer {
+	public static final String MOD_ID = "blowdartmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/mjhines2/blowdartmod/BlowDartModClient.java
@@ -0,0 +1,10 @@
+package net.mjhines2.blowdartmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BlowDartModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
